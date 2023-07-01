218cdf545a7cf2d3b82be252a037d5d1ace1ba57
Sandhiya-R03 <112157222+Sandhiya-R03@users.noreply.github.com>
2023-07-01 08:05:23 +0530

    Update sum&diff.java

--- operators/sum&diff.java
+++ operators/sum&diff.java
@@ -1,1 +1,46 @@
+/ QUESTION:
 
+Write a Java program to get 2 numbers from the user and calculate their sum and difference using '+' and '-' operators respectively.
+
+Print the corresponding sum and difference of the numbers as output in the console.
+
+Input format:
+
+First input: an integer
+
+Second input: an integer
+
+Output format:
+
+First output will be the sum of two integers
+
+Second output will be the difference of two integers
+
+Sample Input:
+
+55
+
+34
+
+Sample Output:
+
+89
+
+21 /
+
+  import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+        {
+            // fill your code
+            //get vals from user for add and sub
+            Scanner sc =new Scanner(System.in);
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            System.out.println(a+b);
+            System.out.println(a-b);
+            
+        }
+        
+}
